5dc3a100f739a1e45ea761182370eb67aa55cfa4
Brian White <mscdex@mscdex.net>
2012-01-08 05:40:04 -0500

    Allow configurable type and length field sizes

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Description
 
 xfer is a module for [node.js](http://nodejs.org/) that provides reading and writing of simple TLV (Type-Length-Value) tuples.
 
-Each tuple has an 8-bit type (0x00 - 0xFF) and a max (data) value size of 65,532 bytes.
+This module allows configurable type and length field sizes and requires that both sender and receiver agree on a type and length size.
 
 
 Requirements
@@ -17,8 +17,11 @@ Example
 
         var net = require('net'), Xfer = require('xfer');
 
+        var TYPE_SIZE = 1, // in bytes, 0 to 255
+            LEN_SIZE = 2; // in bytes, 2 bytes == maximum payload size is 65535 bytes
+
         var server = net.createServer(function(client) {
-          client.xfer = new Xfer(client);
+          client.xfer = new Xfer(TYPE_SIZE, LEN_SIZE, client);
           client.xfer.write(0x20, 'My cool little message! :-)');
           client.xfer.write(0x01, new Buffer([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
         });
@@ -26,7 +29,7 @@ Example
 
         process.nextTick(function() {
           var client = net.createConnection(8118);
-          client.xfer = new Xfer(client);
+          client.xfer = new Xfer(TYPE_SIZE, LEN_SIZE, client);
           client.xfer.on(0x20, function(data) {
             console.error('Got message 0x20: ' + data.toString());
           });
@@ -40,9 +43,3 @@ Example
         // output:
         //    Got message 0x20: My cool little message! :-)
         //    Got message 0x01: <Buffer 01 02 03 04 05 06 07 08 09 0a>
-
-
-TODO
-====
-
-* Convert module to be a stream so you can simply pipe to it?
--- xfer.js
+++ xfer.js
@@ -1,38 +1,65 @@
-var MAX_DATA_SIZE = 65535 - 3; // 3 byte header: 1 byte type + 2 byte length
+var STATE_NEED_TYPE = 0,
+    STATE_NEED_LEN = 1,
+    STATE_DATA = 2;
 
-var Xfer = module.exports = function(stream) {
+var Xfer = module.exports = function(typeLen, sizeLen, stream) {
   var self = this,
-      state = {
-        type: undefined,
-        len: new Array(),
-        recvd: 0,
-        buffer: undefined
-      };
+      state = STATE_NEED_TYPE,
+      type = undefined,
+      nType = 0,
+      len = undefined,
+      nLen = 0,
+      recvd = 0,
+      buffer = undefined;
   this.stream = stream;
+  this.typeBytes = typeLen;
+  this.sizeBytes = sizeLen;
+  this.maxType = Math.pow(2, this.typeBytes * 8) - 1;
+  this.maxPayloadSize = Math.pow(2, this.sizeBytes * 8) - 1;
   stream.on('data', function parse(data) {
     var i = 0, dataLen = data.length;
-    if (typeof state.type === 'undefined')
-      state.type = data[i++];
-    if (i < dataLen && state.len.length < 2)
-      state.len.push(data[i++]);
-    if (i < dataLen && state.len.length < 2)
-      state.len.push(data[i++]);
-    if (i < dataLen) {
+    if (state === STATE_NEED_TYPE) {
+      buffer = len = undefined;
+      recvd = nLen = 0;
+      while (i < dataLen && nType < self.typeBytes) {
+        ++nType;
+        if (type === undefined)
+          type = data[i++];
+        else {
+          type *= 256;
+          type += data[i++];
+        }
+      }
+      if (nType === self.typeBytes)
+        state = STATE_NEED_LEN;
+    }
+    if (state === STATE_NEED_LEN) {
+      while (i < dataLen && nLen < self.sizeBytes) {
+        ++nLen;
+        if (len === undefined)
+          len = data[i++];
+        else {
+          len *= 256;
+          len += data[i++];
+        }
+      }
+      if (nLen === self.sizeBytes)
+        state = STATE_DATA;
+    }
+    if (state === STATE_DATA && i < dataLen) {
       // packet data
       dataLen -= i;
-      if (!state.buffer) {
-        state.len = (state.len[0] << 8) + state.len[1];
-        state.buffer = new Buffer(state.len);
-      }
-      var numBytes = (state.recvd + dataLen > state.len ? state.len - state.recvd : dataLen);
-      data.copy(state.buffer, state.recvd, i, i + numBytes);
-      state.recvd += numBytes;
-      if (state.recvd === state.len) {
-        self.emit(state.type, state.buffer);
-        state.type = undefined;
-        state.len = new Array();
-        state.recvd = 0;
-        state.buffer = undefined;
+      if (!buffer)
+        buffer = new Buffer(len);
+      var numBytes = (recvd + dataLen > len ? len - recvd : dataLen);
+      data.copy(buffer, recvd, i, i + numBytes);
+      recvd += numBytes;
+      if (recvd === len) {
+        var actualType = type;
+        state = STATE_NEED_TYPE;
+        type = undefined;
+        nType = 0;
+        self.emit(actualType, buffer);
       }
       if (numBytes !== dataLen) {
         // extra data in the pipeline
@@ -42,25 +69,32 @@ var Xfer = module.exports = function(stream) {
   });
 };
 require('util').inherits(Xfer, require('events').EventEmitter);
+
 Xfer.prototype.write = function(type, data) {
-  if (data && data.length > MAX_DATA_SIZE)
-    throw new Error('Cannot write data (' + data.length + ' bytes) larger than max allowed size of ' + MAX_DATA_SIZE + ' bytes');
+  if (data && data.length > this.maxPayloadSize)
+    throw new Error('Cannot write data (' + data.length + ' bytes) larger than max allowed size of ' + this.maxPayloadSize + ' bytes');
   else if (!this.stream.writable)
     throw new Error('Cannot write data: stream is no longer writable');
-  else if (typeof type !== 'number' || type < 0 || type > 255)
-    throw new Error('Packet type must be a number between 0 and 255');
-  var len = 0, len1 = 0, len2 = 0, outBuf, newData = data;
-  if (data && data.length > 0) {
-    len = data.length;
-    len1 = (data.length >> 8) & 0xFF;
-    len2 = (data.length & 0xFF);
-  }
-  outBuf = new Buffer(3 + len);
-  outBuf[0] = type;
-  outBuf[1] = len1;
-  outBuf[2] = len2;
-  if (!Buffer.isBuffer(newData))
-    newData = new Buffer(newData);
-  newData.copy(outBuf, 3);
+  else if (typeof type !== 'number' || type < 0 || type > this.maxType)
+    throw new Error('Packet type must be a number between 0 and ' + this.maxType);
+
+  var len = 0, outBuf, p = this.typeBytes;
+
+  if (data)
+    len = (Buffer.isBuffer(data) ? data.length : Buffer.byteLength(data));
+  outBuf = new Buffer(this.typeBytes + this.sizeBytes + len);
+
+  outBuf[this.typeBytes - 1] = type & 0xFF;
+  for (var i = 1; i < this.typeBytes; ++i)
+    outBuf[this.typeBytes - 1 - i] = Math.floor(type / (i * 256)) & 0xFF;
+
+  outBuf[p + (this.sizeBytes - 1)] = len & 0xFF;
+  for (var i = 1; i < this.sizeBytes; ++i)
+    outBuf[p + (this.sizeBytes - 1 - i)] = Math.floor(len / (i * 256)) & 0xFF;
+
+  p += this.sizeBytes;
+
+  if (len)
+    (Buffer.isBuffer(data) ? data : new Buffer(data)).copy(outBuf, p);
   this.stream.write(outBuf);
 };
